674eb008e183859eb52e8cedf35ec2364996afd5
Wellington Garcia <garciawell@gmail.com>
2020-04-22 22:13:26 -0300

    incluindo item

--- relacional/consultarEstados.sql
+++ relacional/consultarEstados.sql
@@ -4,6 +4,6 @@ select nome,sigla as 'Sigla dos estados' from estados
 where regiao = 'Sul'
 
 
-select nome, regiao from estados
+select nome, regiao, populacao from estados
 where populacao >= 10
 order by populacao DESC
